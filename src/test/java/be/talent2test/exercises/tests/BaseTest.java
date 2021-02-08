2271bec29264cd9103b1145e3c26534e8b2739ef
HannesDenOuden <hannes.den.ouden@gmail.com>
2021-02-08 16:09:50 +0100

    Aanpassing met assertions, eerste test, oefening 5 volledig

---
+++ src/test/java/be/talent2test/exercises/tests/BaseTest.java
@@ -0,0 +1,9 @@
+package be.talent2test.exercises.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BaseTest {
+    WebDriver driver;
+}
---
+++ src/test/java/be/talent2test/exercises/tests/FirstTest.java
@@ -0,0 +1,50 @@
+package be.talent2test.exercises.tests;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest extends BaseTest{
+    public void openBrowser()
+    {
+        // System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\OpenJDK\\jdk-15.0.2\\webdrivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Java\\Webdrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+    }
+
+    @BeforeTest
+    public void Initialize() {
+        openBrowser();
+    }
+
+
+    @Test
+    public void detectElements()
+    {
+        WebElement headerlogo = driver.findElement(By.id("header_logo"));
+        Assert.assertTrue(headerlogo.isDisplayed());
+    }
+
+    @Test
+    public void searchDresses()
+    {
+        driver.findElement(By.id("search_query_top")).sendKeys("dress");
+        //String dressPrice = driver.findElement(By.cssSelector("#center_column .product_list li:nth-child(2) > .product-container span[itemprop='price']")).getText();
+        driver.findElement(By.cssSelector("#center_column .product_list > li:nth-child(2)")).click();
+        //String detailDressPrice = driver.findElement(By.id("our_price_display")).getText();
+        //Assert.assertEquals(dressPrice, detailDressPrice);
+    }
+
+    //@AfterTest
+    //public void CleanUp() {driver.quit();}
+}
